8b07667c6c6107844d7841f7b2aef29fca3f4a86
Leon Berisha <leberisha@vsite.hr>
2024-11-21 19:10:18 +0100

    21.11.2024.

--- MVCLibrary/Models/Book.cs
+++ MVCLibrary/Models/Book.cs
@@ -4,6 +4,7 @@
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public string ISBN { get; set; }
-}
+        public string ISBN { get; set;
+        public string Author { get; set; }
+    }
 }
